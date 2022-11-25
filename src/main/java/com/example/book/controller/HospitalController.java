d24edbce5ad0bd028a7f233b03a0c5e38208b82b
Kimkiheon <cbj2741@gmail.com>
2022-11-25 14:43:09 +0900

    ADD: HospitalController - reviewAdd

---
+++ src/main/java/com/example/book/controller/HospitalController.java
@@ -0,0 +1,25 @@
+package com.example.book.controller;
+
+import com.example.book.domain.dto.ReviewRequest;
+import com.example.book.domain.dto.ReviewResponse;
+import com.example.book.service.HospitalService;
+import com.example.book.service.ReviewService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/hospitals")
+public class HospitalController {
+    private final ReviewService reviewService;
+    private final HospitalService hospitalService;
+    public HospitalController(ReviewService reviewService, HospitalService hospitalService) {
+        this.reviewService = reviewService;
+        this.hospitalService = hospitalService;
+    }
+
+    @PostMapping("/{id]/reviews/add")
+    public ResponseEntity<ReviewResponse> add(@PathVariable Long id, @RequestBody ReviewRequest reviewRequest){
+        ReviewResponse reviewResponse = reviewService.add(reviewRequest);
+        return ResponseEntity.ok().body(reviewResponse);
+    }
+}
