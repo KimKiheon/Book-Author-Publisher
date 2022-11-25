a64e720e0aa7a2821d212611676e8a7f99b0b6bf
Kimkiheon <cbj2741@gmail.com>
2022-11-25 14:10:57 +0900

    ADD: Hospital,Publisher,Review Repository

---
+++ src/main/java/com/example/book/repository/HospitalRepository.java
@@ -0,0 +1,7 @@
+package com.example.book.repository;
+
+import com.example.book.domain.entity.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+}
