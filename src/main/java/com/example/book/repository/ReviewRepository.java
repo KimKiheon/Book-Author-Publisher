936aa1b0091214de88056b5f1c55df482fb383d2
Kimkiheon <cbj2741@gmail.com>
2022-11-28 14:25:22 +0900

    ADD: Hospital ID로 리뷰 조회하기

--- src/main/java/com/example/book/repository/ReviewRepository.java
+++ src/main/java/com/example/book/repository/ReviewRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByHospital_Id(Long id);
+
     List<Review> findByHospital(Hospital hospital);
 }
--- src/main/java/com/example/book/service/HospitalService.java
+++ src/main/java/com/example/book/service/HospitalService.java
@@ -1,8 +1,14 @@
 package com.example.book.service;
 
+import com.example.book.domain.dto.HospitalResponse;
+import com.example.book.domain.entity.Hospital;
+import com.example.book.domain.entity.Review;
 import com.example.book.repository.HospitalRepository;
 import com.example.book.repository.ReviewRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HospitalService {
@@ -11,5 +17,10 @@ public class HospitalService {
     public HospitalService(HospitalRepository hospitalRepository, ReviewRepository reviewRepository){
         this.hospitalRepository = hospitalRepository;
         this.reviewRepository = reviewRepository;
+    }
+    public HospitalResponse findHospital(Long id) {
+        Optional<Hospital> hospitals = hospitalRepository.findById(id);
+        List<Review> reviews = reviewRepository.findByHospital_Id(hospitals.get().getId());
+        return new HospitalResponse();
     }
 }
